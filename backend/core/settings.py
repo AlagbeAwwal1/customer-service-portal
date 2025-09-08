c39d7d50f25bd9e9bddbe88a2a573901e3eaac6d
AlagbeAwwal1 <awwal0421@gmail.com>
2025-09-07 22:20:00 -0300

    commit for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,7 +14,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "customer-service-portal-three.vercel
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://customer-service-portal-three.vercel.app",
+    "https://customer-service-portal-three.vercel.app/",
 ]
 
 # If you need credentials (cookies, auth headers)
